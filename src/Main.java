152d29db232d709a11c1dfb81bb7b67121405eeb
yulescrob <122058516+yulescrob@users.noreply.github.com>
2023-02-12 22:49:19 -0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        final int LEGAL_AGE = 21;
+        int userAge;
+
+        //input choice
+        userAge = 30;
+
+        if (userAge >= LEGAL_AGE) //condition if 21 or older
+        {
+            System.out.println("You get a wrist band.");
+        }
+
     }
 }
